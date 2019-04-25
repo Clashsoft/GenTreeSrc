5d95ad2ef9621ee875c7ec76032200d3a241c0bf
Adrian Kunz <clashsoft@hotmail.com>
2019-04-25 14:45:27 +0200

    Add Tool
    * Add a javax.tools.Tool implementation.
    * Updated command-line parsing.

--- src/main/java/de/clashsoft/gentreesrc/Main.java
+++ src/main/java/de/clashsoft/gentreesrc/Main.java
@@ -1,46 +1,11 @@
 package de.clashsoft.gentreesrc;
 
-import java.io.IOException;
+import de.clashsoft.gentreesrc.tool.Tool;
 
 public final class Main
 {
-	// no instances
-	private Main()
+	public static void main(String[] args)
 	{
-	}
-
-	public static void main(String[] args) throws IOException
-	{
-		if (args.length < 2)
-		{
-			printUsage();
-			return;
-		}
-
-		if (!"-l".equals(args[0]))
-		{
-			genTreeSrc(args[0], args[1], "Java");
-			return;
-		}
-
-		if (args.length < 4)
-		{
-			printUsage();
-			return;
-		}
-
-		genTreeSrc(args[2], args[3], args[1]);
-		return;
-	}
-
-	private static void printUsage()
-	{
-		System.err.println("usage: gentreesrc <descriptionFile> <targetDirectory>");
-		System.err.println("     | gentreesrc -l <language> <descriptionFile> <targetDirectory>");
-	}
-
-	public static void genTreeSrc(String descriptionFile, String targetDirectory, String language) throws IOException
-	{
-		Generator.generate(Parser.parse(descriptionFile), targetDirectory, language);
+		System.exit(new Tool().run(System.in, System.out, System.err, args));
 	}
 }
---
+++ src/main/java/de/clashsoft/gentreesrc/tool/Tool.java
@@ -0,0 +1,142 @@
+package de.clashsoft.gentreesrc.tool;
+
+import de.clashsoft.gentreesrc.Generator;
+import de.clashsoft.gentreesrc.Parser;
+import de.clashsoft.gentreesrc.tree.DefinitionFile;
+import org.apache.commons.cli.*;
+
+import javax.lang.model.SourceVersion;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+public class Tool implements javax.tools.Tool
+{
+	// =============== Constants ===============
+
+	private static final String TOOL_NAME      = "gentreesrc";
+	private static final String USAGE          = TOOL_NAME + " <options> <inputDirs...>";
+	private static final String FILE_EXTENSION = ".gts";
+	private static final String HEADER         = "options:";
+
+	// =============== Fields ===============
+
+	private final Config      config = new Config();
+	private       PrintWriter out;
+	private       PrintWriter err;
+
+	// =============== Properties ===============
+
+	public PrintWriter getOut()
+	{
+		return this.out;
+	}
+
+	public void setOut(PrintWriter out)
+	{
+		this.out = out;
+	}
+
+	public PrintWriter getErr()
+	{
+		return this.err;
+	}
+
+	public void setErr(PrintWriter err)
+	{
+		this.err = err;
+	}
+
+	@Override
+	public Set<SourceVersion> getSourceVersions()
+	{
+		return Collections.singleton(SourceVersion.RELEASE_8);
+	}
+
+	// =============== Methods ===============
+
+	@Override
+	public int run(InputStream in, OutputStream out, OutputStream err, String... arguments)
+	{
+		this.setOut(new PrintWriter(out));
+		this.setErr(new PrintWriter(err));
+
+		final int result = this.run(arguments);
+
+		this.getOut().flush();
+		this.getErr().flush();
+
+		return result;
+	}
+
+	private int run(String[] arguments)
+	{
+		Options options = this.config.createOptions();
+
+		CommandLineParser parser = new DefaultParser();
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLine cmd;
+
+		try
+		{
+			cmd = parser.parse(options, arguments);
+		}
+		catch (ParseException e)
+		{
+			this.getOut().println(e.getMessage());
+			formatter.printHelp(this.getOut(), 80, USAGE, HEADER, options, 2, 1, "");
+			return 1;
+		}
+
+		this.config.readOptions(cmd);
+		return this.run();
+	}
+
+	private int run()
+	{
+		for (String inputDirName : this.config.getInputDirs())
+		{
+			final File inputFile = new File(inputDirName);
+			if (inputFile.exists())
+			{
+				this.process(inputFile);
+			}
+		}
+		return 0;
+	}
+
+	private void process(File inputFile)
+	{
+		if (inputFile.isDirectory())
+		{
+			for (File child : Objects.requireNonNull(inputFile.listFiles()))
+			{
+				this.process(child);
+			}
+
+			return;
+		}
+
+		if (inputFile.getName().endsWith(FILE_EXTENSION))
+		{
+			this.processDefinition(inputFile);
+		}
+	}
+
+	private void processDefinition(File inputFile)
+	{
+		try
+		{
+			final DefinitionFile definitionFile = Parser.parse(inputFile.getAbsolutePath());
+			Generator.generate(definitionFile, this.config.getOutputDir(), this.config.getLanguage());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(this.err);
+		}
+	}
+}
